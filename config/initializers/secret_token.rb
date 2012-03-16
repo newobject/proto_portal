05814d9d8554934d4abe56d854288f36d7d5163c
Bo Zhang <object.new@gmail.com>
2012-03-17 00:23:48 +0800

    init rails env and devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProtoPortal::Application.config.secret_token = '76568ec367ef512bb7c8c476ca43991d5e392558468d57030eb08e90a12e279f4955f9dc767b133d06993c086870b5be4a9b9a7ba7b7eb87b53087b42eca4b65'
